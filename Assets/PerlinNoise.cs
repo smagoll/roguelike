4dd313369c6c19176b1aba28e4af0a22d0867676
smagoll <68508178+smagoll@users.noreply.github.com>
2024-02-13 21:09:27 +0500

    add-noise

---
+++ Assets/PerlinNoise.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20;
+
+    private void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    private Texture2D GenerateTexture()
+    {
+        Texture2D texture = new(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    private Color CalculateColor(int x, int y)
+    {
+        float xCoord = x / width * scale;
+        float yCoord = x / width * scale;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
